eb7b96cf039050f567b15902234d2a223d49cd1b
Stefano Castello <steisbetter@outlook.it>
2017-12-21 19:22:51 +0100

    Costruito Database per la TravelCompany Mancano le Fluent API Due entity City e Leg

---
+++ ITravelCompany/ITravelCompany.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITravelCompany
+{
+    public interface ITravelCompany
+    {
+        string Name { get; }
+
+        int CreateLeg(string from, string to, int cost, int distance, TransportType transportType);
+        void DeleteLeg(int legToBeRemoved);
+        // ILedDTO GetLegDTOFromId(int legId);
+    }
+
+    public enum TransportType
+    {
+        None = 0,
+        Plane = 1,
+        Train = 2,
+        Bus = 3,
+        Ship = 8 //TODO 8 not 5 ?
+    }
+}
---
+++ TravelCompanyFakeComponent/TravelCompanyDatabaseContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelCompanyFakeComponent
+{
+    public class TravelCompanyDatabaseContext: DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Leg> Legs { get; set; }
+    }
+
+    public class City
+    {
+        [Key]
+        public string Name { get; set; }
+        public virtual ICollection<Leg> Departures { get; set; }
+    }
+
+    public class Leg
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("From")]
+        public string CityNameFrom { get; set; }
+        public virtual City From { get; set; }
+        [ForeignKey("To")]
+        public string CityNameTo { get; set; }
+        public virtual City To { get; set; }
+        public int Length { get; set; }
+        public int Price { get; set; }
+    }
+}
